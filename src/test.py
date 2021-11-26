18bef2bcc52e8ab4c1b252616a4701e4cbee1a12
Jotebert <jotebeib@gmail.com>
2021-11-26 16:32:52 +0100

    add logger

--- src/test.py
+++ src/test.py
@@ -3,7 +3,9 @@ import pandas as pd
 import numpy as np
 import os
 import itertools
+from more_itertools import one
 import datetime
+import logging
 
 from sklearn.base import BaseEstimator, TransformerMixin
 
@@ -35,6 +37,7 @@ class Base_Transformer(BaseEstimator, TransformerMixin):
     '''
     def __init__(self):
         pass
+        self.__name__ = 'Base_Transformer'
     def fit(self, X):
         return self
     def transform(self, X):
@@ -43,11 +46,13 @@ class Base_Transformer(BaseEstimator, TransformerMixin):
 class DecisionTreeClassifier_Personalized(DecisionTreeClassifier):
     def __init__(self):
         self.is_algorithm = True
+        #self.name = 'DecisionTreeClassifier'
         super().__init__()
 
 class RandomForestClassifier_Personalized(RandomForestClassifier):
     def __init__(self):
         self.is_algorithm = True
+        #self.name = 'RandomForestClassifier'
         super().__init__()
 
 
@@ -81,9 +86,11 @@ class Evaluation_Algorithms():
 
     def get_preprocessing_algorithm_objects(self, pipeline):
         class_objects = [pipeline_class() for pipeline_class in pipeline]
-        algorithm_object = [class_object for class_object in class_objects if hasattr(class_object, 'is_algorithm')]
+        algorithm_object = one([class_object for class_object in class_objects if hasattr(class_object, 'is_algorithm')])
+        logger.info(f'Algorithm used: {algorithm_object.__class__.__name__}')
         preprocessing_objects = [class_object for class_object in class_objects if not hasattr(class_object, 'is_algorithm')]
-        return preprocessing_objects, *algorithm_object
+        logger.info(f'Data Preprocessing Methods used: {[preprocessing_object.__name__ for preprocessing_object in preprocessing_objects]}')
+        return preprocessing_objects, algorithm_object
 
     def perform_preprocessing(self, preprocessing_objects, X, y):
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
@@ -117,16 +124,23 @@ class Evaluation_Algorithms():
         if os.path.isdir(self.dataset_results_folder): pass
         else: os.makedirs(self.dataset_results_folder)
 
+        self.dataset_characteristics_file_path = os.path.join(self.dataset_results_folder, f'{self.dataset_name}_dataset_characteristics.csv')
+        self.evaluation_results_file_path = os.path.join(self.dataset_results_folder, f'{self.dataset_name}_evaluation_results.csv')  
 
     def run(self):
-        
+        logger.info('Start Evaluation')
         X_evaluation, y_evaluation = self.get_dataset()
         self.calculate_combinations_for_evaluation()
+        number_of_combinations = len(self.calculated_combinations)
+        
         for combination in self.calculated_combinations:
+            logger.info(f'Number of combinations to solve: {number_of_combinations}')
             preprocessing_objects, algorithm_object = self.get_preprocessing_algorithm_objects(combination)
             X_train, X_test, y_train, y_test = self.perform_preprocessing(preprocessing_objects, X_evaluation, y_evaluation)
             self.evaluate_algorithm(algorithm_object, X_train, X_test, y_train, y_test)
             print(self.machinelearning_metrics)
+
+            number_of_combinations -= 1
         self.set_output_files()
 
 if __name__ == '__main__':
@@ -136,6 +150,20 @@ if __name__ == '__main__':
     current_results_folder = os.path.join(results_folder, f'results_{now}')
     if os.path.isdir(current_results_folder): pass
     else: os.makedirs(current_results_folder)
+
+    logger = logging.getLogger('Algorithm Evaluation')
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+    fh = logging.FileHandler(os.path.join(current_results_folder,'log.log'))
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+
+    logger.addHandler(ch)
+    logger.addHandler(fh)
 
     benchmark_test = Evaluation_Algorithms(
         current_results_folder,
